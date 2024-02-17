6d11841683bcd3a81755759a9d9a920e3df7ba89
Perla Valdovinos <p_1059@hotmail.com>
2024-02-16 23:48:57 -0700

    Add project files.

---
+++ perlaVHobbies/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace perlaVHobbies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ perlaVHobbies/wwwroot/lib/bootstrap/dist/js/calculator.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $('#calculatorForm').submit(function (e) {
+        e.preventDefault();
+        var hours = parseInt($('#hours').val());
+        var hourlyRate = parseInt($('#hourlyRate').val());
+        if (!isNaN(hours) && hours > 0) {
+            var total = hours * hourlyRate;
+            $('#total').text('Total: $' + total);
+        } else {
+            alert('Please enter a valid number of hours.');
+        }
+    });
+});
